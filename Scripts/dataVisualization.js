8752532cd6081ecf556d64bafdd3e84aa71cdb08
Michael Parkin <1894979@students.wits.ac.za>
2021-09-30 15:56:16 +0200

    Finalized first data visualization

--- Scripts/dataVisualization.js
+++ Scripts/dataVisualization.js
@@ -1,24 +1,5 @@
 const dataBreachURL = "https://haveibeenpwned.com/api/v3/breaches"
 
-const sortData = (data) => {
-    data.sort((a,b) => a.PwnCount - b.PwnCount
-    )
-
-    data.reverse();
-
-    console.log(data);
-
-
-    data.forEach(element => {
-        if (element.IsVerified
-                && count <10
-                    && element.Domain != ""){
-                    count++;
-                    topBreaches.push({"Name": element.Name, "PwnCount": element.PwnCount});
-        }
-    });
-} 
-
 let count = 0;
 
 let topBreaches =[];
@@ -38,14 +19,30 @@ getAllDataBreaches().catch(error =>{
     console.error(error);
 });
 
+const sortData = (data) => {
+    data.sort((a,b) => a.PwnCount - b.PwnCount
+    )
+
+    data.reverse();
+
+    console.log(data);
+
+
+    data.forEach(element => {
+        if (element.IsVerified
+                && count <10
+                    && element.Domain != ""){
+                    count++;
+                    topBreaches.push({"Name": element.Name, "PwnCount": element.PwnCount});
+        }
+    });
+} 
+
 console.log(d3);
 
 const svgArea = d3.select("svg");
-
 const width = svgArea.attr('width');
 const height = svgArea.attr('height');
-
-console.log(svgArea);
 
 const loadBarGraph = (data) => {
 
@@ -53,51 +50,51 @@ const loadBarGraph = (data) => {
     .attr("viewBox", "0 0 750 500")
     .attr("preserveAspectRatio", "xMidYMin meet")
 
-    const margin = {top: 40, right: 100, bottom: 100, left: 200};
+    const margin = {top: 40, right: 75, bottom: 100, left: 225};
     innerWidth = width - margin.right - margin.left;
     innerHeight = height - margin.bottom - margin.top;
 
+    const yScale = d3.scaleBand()
+    .domain(data.map(d => d.Name))
+    .range([0, innerHeight])
+    .padding(0.1);
+
     const xScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.PwnCount)])
-        .range([0, innerWidth]);
-
+    .domain([0, d3.max(data, dataInput => dataInput.PwnCount)])
+    .range([0, innerWidth]);
     
-        const yScale = d3.scaleBand()
-        .domain(data.map(d => d.Name))
-        .range([0, innerHeight])
-        .padding(0.1);
+    const g = svgArea.append('g')
+    .attr('transform', "translate(" + margin.left + "," + margin.top + ")");
 
+    const xAxis = d3.axisBottom().scale(xScale)
+    .tickSize(-innerHeight);
 
-        const g = svgArea.append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`);
+    const yAxis = d3.axisLeft().scale(yScale);
 
-        const xAxis = d3.axisBottom(xScale)
-        .tickSize(-innerHeight);
-
-        g.append('g').call(d3.axisLeft(yScale))
-        .selectAll('.domain, .tick line')
-        .remove();
-        
-        const xAxisG = g.append('g').call(xAxis.tickFormat(d3.format('.3s')))
-        .attr('transform', `translate(0,${innerHeight})`);
-        xAxisG.select('.domain')
-        .remove();;
-        
-
-        console.log(yScale.domain());
+    g.append('g')
+    .call(yAxis)
+    .selectAll('.domain, .tick line')
+    .remove();
+    
+    const xAxisG = g.append('g')
+    .call(xAxis.tickFormat(d3.format('.3s')))
+    .attr('transform', "translate(0," + innerHeight + ")");
+    xAxisG.select('.domain')
+    .remove();
+    
 
     g.selectAll('rect')
         .data(data)
         .enter().append('rect')
-            .attr('y', d => yScale(d.Name))
+            .attr('y', dataInput => yScale(dataInput.Name))
                 .attr('transform', 'translate(100,0)')
-            .attr('width', d => 
-                xScale(d.PwnCount)
+            .attr('width', dataInput => 
+                xScale(dataInput.PwnCount)
             )
             .attr('height', yScale.bandwidth())
             .attr('transform', 'translate(1,0)');
 
 
             xAxisG.append('text').attr('class', 'x-axis-lab')
-            .attr('x', innerWidth/2).attr('y', 45).text('Population');
+            .attr('x', innerWidth/2).attr('y', 45).text('Number of Records Lost');
 }
