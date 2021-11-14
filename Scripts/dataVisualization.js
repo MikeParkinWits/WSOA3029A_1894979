28aff7fb9fabafcf3ea3a4c7519f5b73ea8a39e4
Michael Parkin <1894979@students.wits.ac.za>
2021-11-14 20:17:10 +0200

    Refactoring Code

--- Scripts/dataVisualization.js
+++ Scripts/dataVisualization.js
@@ -27,7 +27,6 @@ const getAllDataBreaches = async () => {
     else if (document.querySelector(".svg-line-graph"))
     {
         groupDataByYear(data);
-        console.log("YAY");
 
         loadLineGraph(numBreachesPerYear);
     }
@@ -37,15 +36,15 @@ const getAllDataBreaches = async () => {
         LoadBubbleGraph(allBreachesInOrder);
     }
 
-    //console.log(data);
 
+    
 }
 
 getAllDataBreaches().catch(error =>{
     console.error(error);
 });
 
-console.log(d3);
+//console.log(d3);
 
 
 
@@ -76,7 +75,7 @@ const sortDataOrder = (data) => {
         }
     });
 
-    console.log(numBreachesPerYear);
+    //console.log(numBreachesPerYear);
 }
 
 const loadBarGraph = (data) => {
@@ -134,7 +133,7 @@ const loadBarGraph = (data) => {
 
 const groupDataByYear = (data) => {
 
-    console.log(data);
+    //console.log(data);
 
     data.forEach(lineChartElement => {
         if (lineChartElement.IsVerified
@@ -150,7 +149,7 @@ const groupDataByYear = (data) => {
         }
     });
 
-    console.log(numBreachesPerYear);
+    //console.log(numBreachesPerYear);
 } 
 
 const loadLineGraph = (data) => {
@@ -172,7 +171,7 @@ const loadLineGraph = (data) => {
         .domain([0, d3.max(data, dataInput => dataInput.DataLoss)])
         .range([height, 0]);
 
-            var g = svgArea.append("g")
+            let g = svgArea.append("g")
             .attr("transform", "translate(" + 100 + "," + 100 + ")");
         
         svgArea.append('text')
@@ -201,7 +200,7 @@ const loadLineGraph = (data) => {
             .call(d3.axisLeft().scale(yScale)
             .tickFormat(customTickFormat))
             
-        console.log(data);
+        //console.log(data);
         svgArea.append('g')
         .selectAll("dot")
         .data(data)
@@ -241,12 +240,11 @@ const orderData = (data) => {
         
     });
 
-    console.log(numBreachesPerYear);
+    //console.log(numBreachesPerYear);
 }
 
 const LoadBubbleGraph = (data) => {
 
-    const file = 'data.json';
     const svgArea = d3.select("svg"),
         margin = {top:1, right: 1, bottom: 1, left: 1},
         width = svgArea.attr('width') - margin.left - margin.right,
@@ -256,253 +254,200 @@ const LoadBubbleGraph = (data) => {
     .attr("viewBox", "0 0 750 500")
     .attr("preserveAspectRatio", "xMidYMin meet");
 
-    const colors = {
-        html: '#F16529',
-        css: '#1C88C7',
-        js: '#FCC700'
-    };
+    const bubble = data => d3.pack()
+    .size([width, height])
+    .padding(2)(d3.hierarchy({ children: data }).sum(d => d.PwnCount));
 
+    //Legend
 
-    const generateChart = data => {
-    
-        const bubble = data => d3.pack()
-            .size([width, height])
-            .padding(2)(d3.hierarchy({ children: data }).sum(d => d.PwnCount));
+    const keys = ["2015 and Later", "2014 and Earlier"]
 
-            console.log(data);
-    
-            /*
-        const svg = d3.select('svg')
-            .style('width', width)
-            .style('height', height);
-            */
-           
-        const root = bubble(data);
-        const tooltip = d3.select('.tooltip');
-    
-        const node = svgArea.selectAll()
-            .data(root.children)
-            .enter().append('g')
-            .attr('transform', `translate(${width / 2}, ${height / 2})`);
+    const color = d3.scaleOrdinal()
+    .domain(keys)
+    .range(d3.schemeSet1);
 
-        node
-            .on('mouseover', function (e, d) {
-                tooltip.select('img').attr('src', d.data.LogoPath);
-                tooltip.select('.company-name').text(d.data.Name);
-                tooltip.select('.tooltip-breach-year').text(d.data.BreachDate);
-                tooltip.select('.tooltip-breach-amount').text(d.data.PwnCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " "));
-                tooltip.select('.tooltip-breach-description').html(d.data.Description);
-                tooltip.style('visibility', 'visible');
-    
-                d3.select(this).select('circle').style('stroke', '#808080');
-                console.log(d3.select(this).size())
+    svgArea.selectAll("legendDots")
+    .data(keys)
+    .enter()
+    .append("circle")
+        .attr("cx", width-150)
+        .attr("cy", function(d,i){ return 20 + i*25})
+        .attr("r", 7)
+        .style("fill", d => color(d))
 
-                /*
-                d3.select(this).raise();
+    svgArea.selectAll("legendLabels")
+    .data(keys)
+    .enter()
+    .append("text")
+        .attr("x", width - 130)
+        .attr("y", function(d,i){ return 20 + i*25})
+        .style("fill", d => color(d))
+        .text(d => d)
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")    
 
-                d3.select(this).selectAll(".test").transition()
-                .ease(d3.easeLinear)
-                .duration(100).style('opacity', 1);
+        //Visualization
+   
+const root = bubble(data);
+const tooltip = d3.select('.tooltip');
 
-                d3.select(this).select(".name").transition()
-                .ease(d3.easeLinear)
-                .duration(100).style('opacity', 0);
+const gElement = svgArea.selectAll()
+    .data(root.children)
+    .enter().append('g')
+    .attr('transform', `translate(${width / 2}, ${height / 2})`);
 
-                d3.select(this).select('circle').transition()
-                .ease(d3.easeLinear)
-                .duration(200)
-                .attr('r', 150);
-                */
-            })
-            .on('mousemove', function (e)
-             {
+    gElement.transition()
+    .ease(d3.easeExpInOut)
+    .duration(1000)
+    .attr('transform', d => `translate(${d.x}, ${d.y})`);
 
-                //console.log(d3.pointer(e));
-                
-                if (window.matchMedia("(max-width: 760px)").matches){
-                    tooltip.style('top', `${550}px`)
-                    .style('left', `${24}px`)
+gElement
+    .on('mouseover', function (e, d) {
+        tooltip.select('img').attr('src', d.data.LogoPath);
+        tooltip.select('.company-name').text(d.data.Name);
+        tooltip.select('.tooltip-breach-year').text(d.data.BreachDate);
+        tooltip.select('.tooltip-breach-amount').text(d.data.PwnCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " "));
+        tooltip.select('.tooltip-breach-description').html(d.data.Description);
+        tooltip.style('visibility', 'visible');
 
-                    console.log(height);
-                }
-                else if (window.matchMedia("(max-width: 1024px)").matches)
-                {
-                    console.log("HELLO!" + window.innerWidth);
+        d3.select(this).select('circle').style('stroke', '#808080');
+        //console.log(d3.select(this).size())
 
-                    tooltip.style('top', `${e.pageY + 50}px`)
-                                         .style('left', `${e.pageX - 100}px`)
-                }
-                else
-                {
-                    console.log("HI!");
+        /*
+        d3.select(this).raise();
 
-                    tooltip.style('top', `${e.pageY + 30}px`)
-                                         .style('left', `${e.pageX - 175}px`)
-                }
-                 
-            })
-            .on('mouseout', function () {
+        d3.select(this).selectAll(".test").transition()
+        .ease(d3.easeLinear)
+        .duration(100).style('opacity', 1);
 
-                /*
-                d3.select(this).select('circle').transition()
-                .ease(d3.easeLinear)
-                .duration(200)
-                .attr('r', d => d.r);
+        d3.select(this).select(".name").transition()
+        .ease(d3.easeLinear)
+        .duration(100).style('opacity', 0);
 
-                d3.select(this).selectAll(".test").transition()
-                .ease(d3.easeLinear)
-                .duration(100).style('opacity', 0);
+        d3.select(this).select('circle').transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .attr('r', 150);
+        */
+    })
+    .on('mousemove', function (e)
+     {        
+        if (window.matchMedia("(max-width: 760px)").matches){
+            tooltip.style('top', '600px')
+            .style('left', '24px')
 
-                d3.select(this).select(".name").transition()
-                .ease(d3.easeLinear)
-                .duration(100).style('opacity', 1);
+        }
+        else if (window.matchMedia("(max-width: 1024px)").matches)
+        {
+            tooltip.style('top', `${e.pageY + 50}px`)
+                                 .style('left', `${e.pageX - 100}px`)
+        }
+        else
+        {
+            tooltip.style('top', `${e.pageY + 30}px`)
+                                 .style('left', `${e.pageX - 175}px`)
+        }
+         
+    })
+    .on('mouseout', function () {
 
-                */
-                
-                
-                d3.select(this).select('circle').style('stroke', 'none');
-                return tooltip.style('visibility', 'hidden');
-            })
+        /*
+        d3.select(this).select('circle').transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .attr('r', d => d.r);
+
+        d3.select(this).selectAll(".test").transition()
+        .ease(d3.easeLinear)
+        .duration(100).style('opacity', 0);
+
+        d3.select(this).select(".name").transition()
+        .ease(d3.easeLinear)
+        .duration(100).style('opacity', 1);
+
+        */
         
+        d3.select(this).select('circle').style('stroke', 'none');
+        return tooltip.style('visibility', 'hidden');
+    })
 
-        const circle = node.append('circle')
-            .style('fill', function(d) { 
+
+const circle = gElement.append('circle')
+    .style('fill', function(d) { 
+        
+        //console.log(d.data.BreachDate);
+
+        if (d.data.BreachDate > 2015){
+            return d3.schemeSet1[0];
+        }else{
+            return d3.schemeSet1[1];
+        }                
+    })
+    .attr('height', d => d.data.PwnCount);
+
+    circle.transition()
+    .ease(d3.easeExpInOut)
+    .duration(1000)
+    .attr('r', d => d.r);
+    
+    //circle.append('text').select('.test');
+
+    /*
+    const label2 = gElement.append('text')
+    .attr('y', -70)
+    .attr('x', 0)
+    .attr('width', '1rem')
+    .attr('height', '1rem')
+    .attr("text-anchor", "middle")
+    .text(d => d.data.name).style('opacity', 0)
+    .attr('class', 'test');
+
+    const label3 = gElement.append('text')
+    .attr('y', -50)
+    .attr('x', 0)
+    .attr("text-anchor", "middle")
+    .text(d => d.data.score).style('opacity', 0)
+    .attr('class', 'test');
+
+    const label4 = gElement.append('text')
+    .attr('y', -20)
+    .attr('x', 0)
+    .attr("text-anchor", "middle")
+    .text(d => d.data.category).style('opacity', 0)
+    .attr('class', 'test');
+             
+    */
                 
-                console.log(d.data.BreachDate);
 
-                if (d.data.BreachDate > 2015){
-                    return d3.schemeSet1[0];
-                }else{
-                    return d3.schemeSet1[1];
-                }                
-            })
-            .attr('height', d => d.data.PwnCount);
+const label = gElement.append('text')
+    .attr('y', d => d.r/9)
+    .attr("text-anchor", "middle")
+    .style("color", "white")
+    .text(d => d.data.Name)
+    .style("font-size", bubbleLabelFontSize)
+    .style('opacity', 0)
+    .attr('class', 'name')
+    .attr("margin", d => d.r);
 
+    label.transition()
+    .delay(700)
+    .ease(d3.easeExpInOut)
+    .duration(1000)
+    .style('opacity', 1)
 
-            
-            //circle.append('text').select('.test');
-
-            /*
-            const label2 = node.append('text')
-            .attr('y', -70)
-            .attr('x', 0)
-            .attr('width', '1rem')
-            .attr('height', '1rem')
-            .attr("text-anchor", "middle")
-            .text(d => d.data.name).style('opacity', 0)
-            .attr('class', 'test');
-
-            const label3 = node.append('text')
-            .attr('y', -50)
-            .attr('x', 0)
-            .attr("text-anchor", "middle")
-            .text(d => d.data.score).style('opacity', 0)
-            .attr('class', 'test');
+    function bubbleLabelFontSize(d) {
+        let xPad = 8,
+            diam = 2 * d.r,
+            labelAvailableWidth = diam - xPad,
+            labelWidth = this.getComputedTextLength();
       
-            const label4 = node.append('text')
-            .attr('y', -20)
-            .attr('x', 0)
-            .attr("text-anchor", "middle")
-            .text(d => d.data.category).style('opacity', 0)
-            .attr('class', 'test');
-                     
-            */
-                        
-        
-        const label = node.append('text')
-            .attr('y', d => d.r/9)
-            .attr("text-anchor", "middle")
-            .style("color", "white")
-            .text(d => d.data.Name)
-            .style("font-size", adaptLabelFontSize)
-            .style('opacity', 0)
-            .attr('class', 'name')
-            .attr("margin", d => d.r);
+        if (labelWidth < labelAvailableWidth) {
+          return null;
+        }
 
-
-
-            function adaptLabelFontSize(d) {
-                var xPadding, diameter, labelAvailableWidth, labelWidth;
-              
-                xPadding = 8;
-                diameter = 2 * d.r;
-                labelAvailableWidth = diameter - xPadding;
-              
-                labelWidth = this.getComputedTextLength();
-              
-                // There is enough space for the label so leave it as is.
-                if (labelWidth < labelAvailableWidth) {
-                  return null;
-                }
-
-                if ((labelAvailableWidth / labelWidth) < 0) {
-                    return '0em';
-                  }
-              
-
-
-                /*
-                 * The meaning of the ratio between labelAvailableWidth and labelWidth equaling 1 is that
-                 * the label is taking up exactly its available space.
-                 * With the result as `1em` the font remains the same.
-                 *
-                 * The meaning of the ratio between labelAvailableWidth and labelWidth equaling 0.5 is that
-                 * the label is taking up twice its available space.
-                 * With the result as `0.5em` the font will change to half its original size.
-                 */
-                
-                console.log(labelAvailableWidth / labelWidth);
-                return (labelAvailableWidth / labelWidth) + 'em';
-              }
-    
-        node.transition()
-            .ease(d3.easeExpInOut)
-            .duration(1000)
-            .attr('transform', d => `translate(${d.x}, ${d.y})`);
-        
-        circle.transition()
-            .ease(d3.easeExpInOut)
-            .duration(1000)
-            .attr('r', d => d.r);
-        
-        label.transition()
-            .delay(700)
-            .ease(d3.easeExpInOut)
-            .duration(1000)
-            .style('opacity', 1)
-
-    };
-
-// create a list of keys
-const keys = ["2015 and Later", "2014 and Earlier"]
-
-// Usually you have a color scale in your chart already
-const color = d3.scaleOrdinal()
-  .domain(keys)
-  .range(d3.schemeSet1);
-
-// Add one dot in the legend for each name.
-svgArea.selectAll("mydots")
-  .data(keys)
-  .enter()
-  .append("circle")
-    .attr("cx", width-150)
-    .attr("cy", function(d,i){ return 20 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
-    .attr("r", 7)
-    .style("fill", function(d){ return color(d)})
-
-// Add one dot in the legend for each name.
-svgArea.selectAll("mylabels")
-  .data(keys)
-  .enter()
-  .append("text")
-    .attr("x", width - 130)
-    .attr("y", function(d,i){ return 20 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
-    .style("fill", function(d){ return color(d)})
-    .text(function(d){ return d})
-    .attr("text-anchor", "left")
-    .style("alignment-baseline", "middle")
-    generateChart(data);
-    
-
+        if ((labelAvailableWidth / labelWidth) < 0) {
+            return '0em';
+        }
+              return (labelAvailableWidth / labelWidth) + 'em';
+      }
 }
